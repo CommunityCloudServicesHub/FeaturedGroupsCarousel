ef3caedcc5ae34550e6c10a03a514f5514500b88
Tarren Anderson <tarren.anderson@salesforce.com>
2017-04-03 18:49:17 -0500

    Component controller and screenshots

--- src/aura/GroupCarouselWrapper/GroupCarouselWrapperController.js
+++ src/aura/GroupCarouselWrapper/GroupCarouselWrapperController.js
@@ -1,10 +1,2 @@
 ({
-	doInit : function(component, event, helper) {
-		// Load Carousel Type Metadata
-		helper.loadSliderTemplates(component);
-		var slideOptions = {'aspectRatio': component.get("v.aspectRatio")};
-		console.log('In init of content carousel');
-		console.log(slideOptions);
-		component.set("v.slideOptions", slideOptions);
-	}
 })
